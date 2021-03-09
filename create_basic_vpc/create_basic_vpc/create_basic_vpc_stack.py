e468eedb4482e32e1008b09d8973732d2e0996f6
Dragos Stan <dragos.stan2602@gmail.com>
2021-03-09 15:06:31 +0100

    New Resources

--- create_basic_vpc/create_basic_vpc/create_basic_vpc_stack.py
+++ create_basic_vpc/create_basic_vpc/create_basic_vpc_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import core
+from aws_cdk import aws_ec2 as ec2
 
 
 class CreateBasicVpcStack(core.Stack):
@@ -7,3 +8,49 @@ class CreateBasicVpcStack(core.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        vpc = ec2.Vpc(self, "MyVPC",
+                      nat_gateways=0,
+                      cidr="192.168.0.0/20",
+                      max_azs=1,
+                      subnet_configuration=[], )
+
+        app_subnet = ec2.CfnSubnet(self, "Application",
+                                   vpc_id=vpc.vpc_id,
+                                   availability_zone="eu-central-1a",
+                                   cidr_block="192.168.1.0/24",
+                                   map_public_ip_on_launch=False,
+                                   tags=[core.CfnTag(key="Name", value="Application")])
+
+        web_subnet = ec2.CfnSubnet(self, "WebHost",
+                                   vpc_id=vpc.vpc_id,
+                                   availability_zone="eu-central-1b",
+                                   cidr_block="192.168.2.0/24",
+                                   map_public_ip_on_launch=True,
+                                   tags=[core.CfnTag(key="Name", value="WebHost")])
+
+        private_rt = ec2.CfnRouteTable(self, id="Private_RT",
+                                       vpc_id=vpc.vpc_id,
+                                       tags=[core.CfnTag(key="Name", value="Private_RT")])
+
+        public_rt = ec2.CfnRouteTable(self, id="Public_RT",
+                                      vpc_id=vpc.vpc_id,
+                                      tags=[core.CfnTag(key="Name", value="Public_RT")])
+
+        ec2.CfnSubnetRouteTableAssociation(self, id="WebHostRTAssoc",
+                                           subnet_id=web_subnet.ref,
+                                           route_table_id=public_rt.ref)
+        ec2.CfnSubnetRouteTableAssociation(self, id="ApplicationRTAssoc",
+                                           subnet_id=app_subnet.ref,
+                                           route_table_id=private_rt.ref)
+
+        igw = ec2.CfnInternetGateway(self, id="MyIGW",
+                                     tags=[core.CfnTag(key="Name", value="Public_RT")])
+
+        ec2.CfnVPCGatewayAttachment(self, id="IGW_Assoc",
+                                    vpc_id=vpc.vpc_id,
+                                    internet_gateway_id=igw.ref)
+
+        default_route = ec2.CfnRoute(self, id="DefaultRoute",
+                                     route_table_id=public_rt.ref,
+                                     destination_cidr_block="0.0.0.0/0",
+                                     gateway_id=igw.ref)
